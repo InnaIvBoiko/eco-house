cf8764a9ffcbcdf83cefe263000b7a2b05664767
Inna Boiko <146464243+InnaIvBoiko@users.noreply.github.com>
2025-09-03 20:28:44 +0200

    Add new icon components for homepage
    - Introduced IconHomePage1.jsx with a detailed SVG representation.
    - Introduced IconHomePage2.jsx featuring a unique design.
    - Introduced IconHomePage3.jsx with a distinct visual style.
    - Introduced IconHomePage4.jsx showcasing another variation of the homepage icon.

--- src/components/Sections/Advantages.jsx
+++ src/components/Sections/Advantages.jsx
@@ -1,4 +1,8 @@
 import styled from 'styled-components';
+import IconHomePage1 from '../Icons/IconHomePage1';
+import IconHomePage2 from '../Icons/IconHomePage2';
+import IconHomePage3 from '../Icons/IconHomePage3';
+import IconHomePage4 from '../Icons/IconHomePage4';
 
 export default function Advantages() {
   return (
@@ -30,6 +34,19 @@ export default function Advantages() {
           Ми створюємо простір, де сучасний дизайн і екологічність поєднуються з турботою про мешканців. Тут є все для
           комфортного життя — від тиші й чистого повітря до якісного будівництва та безпечного середовища.
         </p>
+        <FlexSpaceBetween>
+          <FlexColumn>
+            <AdvantagesListIcons>
+              <li> <IconHomePage1 /> <p><span>Енергоефективність</span> - економія до 50% енергії</p></li>
+              <li> <IconHomePage2 /> <p><span>Мобільність</span> - встановлюється за один тиждень</p></li>
+              <li> <IconHomePage3 /> <p><span>Екологічність</span> - натуральні матеріали та сонячні панелі</p></li>
+              <li> <IconHomePage4 /> <p><span>Доступність</span> - відчутно дешевше за традиційне житло</p></li>
+            </AdvantagesListIcons>
+            <img src="/public/images/homePage/img2.jpg" alt="House" width={801} height={342} />
+            <p><span>Еко</span>Гніздо - це благодійний проєкт, що допомагає створювати доступне та екологічне житло. Ми поєднуємо сучасні технології, стильний дизайн та продумані планування, щоб ваш новий дім був затишним та безпечним</p>
+          </FlexColumn>
+          <img src="/public/images/homePage/img1.jpg" alt="House" width={567} height={884}/>
+        </FlexSpaceBetween>
       </AdvantagesContent>
     </section>
   );
@@ -90,10 +107,45 @@ const AdvantagesContent = styled.div`
     font-weight: 500;
     font-size: 24px;
     color: #333;
+
+    span {
+      color: #006837;
+      font-weight: 600;
+    }
   }
 `;
 
 const FlexSpaceBetween = styled.div`
   display: flex;
   justify-content: space-between;
+`;
+
+const FlexColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 60px;
+  justify-content: space-between;
+`;
+
+const AdvantagesListIcons = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 22px;
+  width: 566px;
+  padding-left: 118px;
+
+  li {
+    display: flex;
+    align-items: center;
+    gap: 16px;
+
+    svg {
+      min-width: 60px;
+      min-height: 60px;
+    }
+
+    p {
+      margin: 0;
+    }
+  }
 `;
