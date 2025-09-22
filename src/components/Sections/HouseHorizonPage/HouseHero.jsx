3fe1866c03debba68c3734ea73f50005f538dc75
Inna Boiko <146464243+InnaIvBoiko@users.noreply.github.com>
2025-09-22 08:08:17 +0200

    Add HouseHero component with styled components and responsive design
    - Implement HouseHero component featuring eco-friendly house attributes with icons and styled layout.
    
    - Ensure all components are styled using styled-components for consistent design and responsiveness across different screen sizes.

---
+++ src/components/Sections/HouseHorizonPage/HouseHero.jsx
@@ -0,0 +1,266 @@
+import styled from 'styled-components';
+import IconHousePage1 from '../../Icons/IconsHousePage/IconHousePage1';
+import IconHousePage2 from '../../Icons/IconsHousePage/IconHousePage2';
+import IconHousePage3 from '../../Icons/IconsHousePage/IconHousePage3';
+import IconHousePage4 from '../../Icons/IconsHousePage/IconHousePage4';
+
+
+export default function HouseHero() {
+    return (
+        <SectionHeroHouse>
+            
+        <h1>
+          <span>Еко</span>Гніздо Горизонт
+        </h1>
+            <List>
+                <ListItemHouse><SvgWrapper><IconHousePage1 /></SvgWrapper>
+                  <div>
+                    <h4>Дах</h4><p>натуральні матеріали</p>
+                  </div>
+                </ListItemHouse>
+                <ListItemHouse> <SvgWrapper><IconHousePage2/></SvgWrapper>
+                  <div>
+                    <h4>Стіни</h4><p>екологічні SIP-панелі</p>
+                  </div>
+                 </ListItemHouse>
+                <ListItemHouse> <SvgWrapper><IconHousePage3/></SvgWrapper>
+                   <div>
+                     <h4>Вікна</h4><p>енергоефективні склопакети</p>
+                   </div>
+                </ListItemHouse>
+                <ListItemHouse><SvgWrapper><IconHousePage4/></SvgWrapper>
+                   <div>
+                     <h4>Двері</h4> <p>із захистом від втрати тепла</p>
+                    </div>
+               </ListItemHouse>
+            </List>
+        </SectionHeroHouse>
+    );
+}
+const SectionHeroHouse = styled.section`
+  position: relative;
+  width: 1440px;
+  height: 666px;
+  background-image: url('/images/houseHorizonPage/house.jpg');
+  background-size: cover;
+
+
+  h1 {
+    font-family: "Inter", sans-serif;
+    font-weight: 600;
+    font-size: 100px;
+    leading-trim: NONE;
+    line-height: 100%;
+    letter-spacing: 0%;
+    color:  #333;
+    text-align: center;
+
+
+    span {
+      font-weight: 600;
+      font-size: 100px;
+      color: #006837;
+    }
+  }
+
+  h4 {
+   font-weight: 500;
+   font-size: 24px;
+   color: #000;
+    leading-trim: NONE;
+    line-height: 100%;
+    letter-spacing: 0%;
+    color: #000000;
+   
+  }
+    p {
+    font-family: "Inter", sans-serif;
+    font-weight: 500;
+    font-size: 16px;
+    color: #333;
+    }
+
+    @media (max-width: 1439px) {
+    background-image: url('/images/houseHorizonPage/house-tab.jpg');
+    width: 744px;
+    height: 666px;
+
+     h1 {
+      font-weight: 600;
+      font-size: 64px;
+      padding-top: 36px;
+    
+
+    span {
+      font-weight: 600;
+      font-size: 64px;
+    }
+  }
+    h4 {
+    font-weight: 500;
+    font-size: 24px;}
+  }
+  
+  @media (max-width: 743px){
+    width: 320px;
+    height: 350px;
+    background-image: url('/images/houseHorizonPage/house-mob.jpg');
+
+    h1 {
+    font-weight: 600;
+    font-size: 32px;
+    padding-top: 20px;
+    
+
+    span {
+      font-weight: 600;
+      font-size: 32px;
+    }
+}
+  h4 {
+    font-weight: 500;
+    font-size: 18px;
+  }
+    p{
+    font-weight: 500;
+    font-size: 12px;
+    }
+
+    }
+`;
+
+
+
+
+const List = styled.ul`
+  position: absolute;
+  max-width: 260px;
+  height: 83px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 16px;
+
+  @media (max-width: 743px) {
+  max-width: 150px;
+
+  max-height: 72px;
+  }
+`;
+
+
+const ListItemHouse = styled.li`
+  border-radius: 10px;
+  height: 83px;
+  padding: 8px 24px;
+  backdrop-filter: blur(3px);
+  background: rgba(251, 254, 251, 0.4);
+  font-weight: 500;
+  color: #333;
+  display: flex;
+  flex-direction: row;
+  gap: 24px;
+  align-items: center;
+  justify-content: center;
+ 
+&:nth-child(1) {
+    position: relative;
+    top: 150px;
+    left: 400px;
+    width: 209px;
+    
+  }
+  &:nth-child(2) {
+    position: relative;
+    top: 276px;
+    left: 856px;
+    width: 205px;
+  }
+  &:nth-child(3) {
+    position: relative;
+    top: 18px;
+    left: 892px;
+    width: 257px;
+  }
+  &:nth-child(4) {
+    position: relative;
+    top: 100px;
+    left: 516px;
+    width: 260px;
+  }
+
+ @media (max-width: 1439px) {
+    font-size: 16px;
+
+    &:nth-child(1) {
+      top: 132px;
+      left: 100px;
+    }
+    &:nth-child(2){
+      top: 286px;
+      left: 470px;
+    }
+    &:nth-child(3){
+      top: -4px;
+      left: 452px;
+    }
+    &:nth-child(4) {
+      top: 72px;
+      left: 146px;
+    }
+
+    }
+
+  @media (max-width: 743px){
+
+  height: 72px;
+  padding: 2px 4.5px;
+  gap: 12px;
+    &:nth-child(1) {
+      width: 116px;
+      height: 56px;
+      top: 46px;
+      left: 20px;
+    }
+    &:nth-child(2){
+      width: 127px;
+      height: 57px;
+      top: 116px;
+      left: 182px;
+
+    }
+    &:nth-child(3){
+      width: 150px;
+      height: 68px;
+      top: -104px;
+      left: 146px;
+    }
+    &:nth-child(4) {
+      width: 124px;
+      height: 72px;
+      top: -60px;
+      left: 16px;
+    }
+     }
+  }
+`;
+
+const SvgWrapper = styled.div`
+  width: 50px;
+  height: 50px;
+
+  svg {
+    width: 50px;
+    height: 50px;
+  }
+
+  @media (max-width: 743px){
+    width: 30px;
+    height: 30px;
+    
+    svg {
+      width: 30px;
+      height: 30px;
+    }
+  }
+
+`;
---
+++ src/components/Sections/HouseHorizonPage/HousePlan.jsx
@@ -0,0 +1,778 @@
+import { useState } from "react";
+import styled from "styled-components";
+import { ChevronLeft, ChevronRight } from "lucide-react"; 
+import IconHousePage9 from '../../Icons/IconsHousePage/IconHousePage9';
+
+
+export default function HousePlan() {
+  const images = [
+    { src: "/images/houseHarmonyPage/Harmony1.jpg", alt: "Вид спереду"  },
+    { src: "/images/houseHarmonyPage/Harmony2.jpg", alt: "Бічний фасад" },
+    { src: "/images/houseHarmonyPage/Harmony3.jpg", alt: "Задній фасад" },
+  ];
+  const [currentIndex, setCurrentIndex] = useState(0);
+  
+
+    const prevSlide = () => {
+    setCurrentIndex((prev) => (prev === 0 ? images.length - 1 : prev - 1));
+  };
+
+  const nextSlide = () => {
+    setCurrentIndex((prev) => (prev === images.length - 1 ? 0 : prev + 1));
+  };
+    return (
+        <SectionHousePlan>
+            <TitleSection>
+                Атмосфера будинку
+            </TitleSection>
+            <MainImage>
+          <button className="nav-btn left" onClick={prevSlide}>
+            <ChevronLeft size={38} />
+           </button>
+
+        <img src={images[currentIndex].src} alt={images[currentIndex].alt} />
+
+        <button className="nav-btn right" onClick={nextSlide}>
+          <ChevronRight size={38} />
+        </button>
+            </MainImage>
+        <Thumbnails>
+          {images.map((img, i) => (
+            <img
+            key={i}
+            src={img.src}
+            alt={img.alt}
+            onClick={() => setCurrentIndex(i)}
+            className={i === currentIndex ? "active" : ""}
+          />
+        ))}
+            </Thumbnails>
+
+            <PlanSection>
+                <h3>План будинку</h3>
+                <img src="/images/houseHarmonyPage/housePlan.jpg" alt="План будинку" width="1036" height="611" />
+            </PlanSection>
+
+        <HousePriceInfo>
+          <h2>Що входить у вартість будинку</h2>
+          <FirstImage src="/images/houseHarmonyPage/FullInfoSection1.png" alt="Будинок"/>
+          <SecondImage src="/images/houseHarmonyPage/FullInfoSection2.png" alt="Будинок"/>
+          <FirstCardList>
+            <h3><IconHousePage9/> Конструкція та матеріали </h3>
+            <ul>
+              <li>Фундамент: гвинтові палі (не шкодять ґрунту, швидкий монтаж).</li>
+              <li>Каркас: метал + дерев’яні елементи з сертифікованої деревини.</li>
+              <li>Стіни: SIP-панелі з утепленням (екологічні матеріали).</li>
+              <li>Дах: натуральні покрівельні матеріали, підготовка для сонячних панелей.</li>
+              <li>Вікна: енергоефективні склопакети класу А+.</li>
+              <li>Двері: утеплені, з підвищеною безпекою.</li>
+            </ul>
+          </FirstCardList>
+          <SecondCardList>
+            <h3><IconHousePage9/> Внутрішнє оздоблення </h3>
+            <ul>
+              <li>Стінові панелі з екологічних матеріалів.</li>
+              <li>Підлога: ламінат/вініл високої міцності.</li>
+              <li>LED-освітлення.</li>
+              <li>Вбудовані розетки та вимикачі.</li>
+            </ul>
+          </SecondCardList>
+          <ThirdCardList>
+            <h3><IconHousePage9/> Інженерія </h3>
+            <ul>
+              <li>Система вентиляції з рекуперацією.</li>
+              <li>Електропроводка (готова для підключення).</li>
+              <li>Точки для підключення кухні та санвузла.</li>
+              <li>Опалення (інфрачервоні панелі або тепловий насос — залежно від комплектації).</li>
+            </ul>
+          </ThirdCardList>
+          <FourthCardList>
+            <h3><IconHousePage9/> Додатково </h3>
+            <ul>
+              <li>Базові меблі (шафа-купе, кухня-мінімум).</li>
+              <li>Санвузол з душовою кабіною.</li>
+              <li>Гарантія: 5 років на конструкцію, 2 роки на обладнання.</li>
+            </ul>
+          </FourthCardList>
+          <FifthCardList>
+            <h3><IconHousePage9/> Доставка та монтаж </h3>
+            <ul>
+              <li>Доставка по Україні включена у вартість.</li>
+              <li>Монтаж під ключ — 3–7 днів (залежно від ділянки).</li>
+              <li>Підключення до мереж (електрика, вода, каналізація — за наявності на ділянці).</li>
+            </ul>
+          </FifthCardList>
+        </HousePriceInfo>
+        <BtnLeaveAppl>Залишити заявку</BtnLeaveAppl>
+        
+        </SectionHousePlan>
+    );
+}
+
+const SectionHousePlan = styled.div`
+padding: 60px 24px;
+  @media (max-width: 1439px) {
+    width: 744px;
+    padding:100px 24px;
+ 
+  }
+  @media (max-width: 743px){
+  width: 320px;
+  padding: 60px 16px;
+  }
+`
+
+const TitleSection = styled.h3`
+font-weight: 600;
+font-size: 48px;
+color: #000;
+margin-bottom: 60px;
+
+@media (max-width: 1439px) {
+
+}
+  @media (max-width: 743px) {
+  font-weight: 600;
+  font-size: 24px;
+  text-align: center;
+  margin-bottom: 24px;
+  }
+
+`
+
+const MainImage = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 24px;
+  position:relative;
+
+  img {
+    width: 1391px;
+    height: 800px;
+    object-fit: cover;
+    border-radius: 10px;
+
+  }
+      .nav-btn {
+    position: absolute;
+    top: 400px;
+    transform: translateY(-50%);
+    background: rgba(251, 254, 251, 0.4);  
+    backdrop-filter: blur(5px);
+    border: none;
+    cursor: pointer;
+    border-radius: 10%;
+    transition: background 0.3s ease;
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    padding: 11px;
+  }
+
+  .nav-btn:hover {
+    background: rgba(251, 254, 251, 0.6); ;
+  }
+
+  .left {
+    left: 60px;
+  }
+
+  .right {
+    right: 60px;
+  }
+
+  @media (max-width: 1439px){
+
+  margin-bottom: 24px;
+  img {
+    width: 696px;
+    height: 483px;
+  }
+    .nav-btn {
+    top: 250px;
+    }
+    .left {
+    left: 40px;
+  }
+
+  .right {
+    right: 40px;
+  }
+  }
+  @media (max-width: 743px) {
+  margin-bottom: 12px;
+  img {
+    width: 288px;
+    height: 222px;
+  }
+    .nav-btn {
+    top: 120px;
+    width: 35px;
+    height: 35px;
+    }
+    .left {
+    left: 5px;
+  }
+
+  .right {
+    right: 5px;
+  }
+  }
+  }
+`;
+
+const Thumbnails = styled.div`
+  display: flex;
+  gap: 20px;
+  margin-bottom: 60px;
+
+  img {
+    border-radius: 10px;
+    width: 448px;
+    height: 320px;
+    object-fit: cover;
+    border-radius: 10px;
+    cursor: pointer;
+    opacity: 1;
+    transition: transform 0.3s ease;
+  }
+
+
+img:hover {
+ 
+  filter: brightness(50%); 
+  transform: scale(1.05);
+}
+
+img.active {
+  filter: brightness(50%);
+  transform: scale(1.05);
+ 
+}
+
+@media (max-width: 1439px){
+  img {
+    width: 217px;
+    height: 201px;
+  }
+
+@media (max-width: 743px){
+    gap: 9px;
+    margin-bottom: 24px;
+
+  img {
+    width: 90px;
+    height: 90px;
+  }
+    }
+`;
+
+const PlanSection = styled.div`
+display: flex;
+gap: 17px;
+margin-bottom: 60px;
+
+h3{
+font-weight: 600;
+font-size: 48px;
+color: #000;
+}
+@media (max-width: 1439px) {
+gap: 0;
+
+h3{
+font-weight: 600;
+font-size: 36px;
+}
+img {
+width: 442px;
+height: 281px;
+}
+
+@media (max-width: 743px){
+flex-wrap: wrap;
+gap: 12px;
+
+
+h3{
+font-weight: 600;
+font-size: 24px;
+}
+img {
+width: 243px;
+height: 145px;
+}
+}
+}
+`;
+
+const HousePriceInfo = styled.div`
+position: relative;
+min-height: 1486px;
+
+h2 {
+font-weight: 600;
+font-size: 48px;
+color: #000;
+}
+@media (max-width: 1439px){
+min-height: 1379px;
+h2 {
+font-weight: 600;
+font-size: 36px;
+}
+}
+@media (max-width: 743px){
+min-height: 1166px;
+h2{
+font-weight: 600;
+font-size: 24px;
+}
+}
+
+`
+
+const FirstImage = styled.img`
+  position:absolute;
+  left: -24px;
+  top: 146px;
+  width: 560px;
+
+  @media (max-width: 1439px){
+    left: -90px;
+    top: 238px;
+    width: 320px;
+  }
+  @media (max-width: 743px){
+    left: -112px;
+    top: 398px;
+    width: 162px;
+  }
+`
+
+const SecondImage = styled.img`
+  position:absolute;
+  right: 0;
+  bottom: 40px;
+  width: 450px;
+  oppacity: 1;
+
+  @media (max-width: 1439px){
+    right: -216px;
+    bottom: 130px;
+    width: 450px;
+  }
+  @media (max-width: 743px){
+    left: 248px;
+    bottom: 178px;
+    width: 188px;
+  }
+`
+
+const FirstCardList = styled.div`
+position: absolute;
+left: 530px;
+top: 130px;
+width: 828px;
+height: 301px;
+
+
+h3{
+font-weight: 500;
+font-size: 48px;
+color: #006837;
+margin-bottom: 40px;
+
+display:flex;
+align-items:center;
+justify-content: flex-start;
+gap: 24px;
+
+}
+ul{
+list-style-type: inherit;
+padding-left: 50px;
+}
+
+li{
+font-weight: 400;
+font-size: 24px;
+color: #333;
+}
+@media (max-width: 1439px) {
+width: 534px;
+height: 314px;
+
+left: 165px;
+top: 90px;
+
+h3 {
+font-size: 32px;
+margin-bottom: 24px;
+}
+
+li{
+font-size: 20px;
+}
+}
+@media (max-width: 743px){
+width: 263px;
+height: 319px;
+
+left: 45px;
+top: 90px;
+
+h3 {
+font-size: 20px;
+margin-bottom: 16px;
+gap: 20px;
+
+svg{
+width: 32px;
+height: 32px;
+}
+}
+
+ul{
+padding-left: 25px;
+}
+
+li{
+font-size: 14px;
+}
+}
+
+
+`
+
+const SecondCardList = styled.div`
+position: absolute;
+right: 120px;
+top: 530px;
+width: 620px;
+height: 214px;
+
+
+h3{
+font-weight: 500;
+font-size: 48px;
+color: #006837;
+margin-bottom: 40px;
+
+display:flex;
+align-items:center;
+justify-content: flex-start;
+gap:24px;
+
+}
+ul{
+list-style-type: inherit;
+padding-left: 50px;
+}
+
+li{
+font-weight: 400;
+font-size: 24px;
+color: #333;
+}
+
+@media (max-width: 1439px) {
+width: 446px;
+height: 170px;
+
+right: -17px;
+top: 448px;
+
+h3 {
+font-size: 32px;
+margin-bottom: 24px;
+}
+
+li{
+font-size: 20px;
+}
+}
+
+@media (max-width: 743px){
+width: 263px;
+
+
+right: -25px;
+top: 424px;
+
+h3 {
+font-size: 20px;
+margin-bottom: 16px;
+gap: 20px;
+
+svg{
+width: 32px;
+height: 32px;
+}
+}
+ul{
+padding-left: 25px;
+}
+li{
+font-size: 14px;
+}
+}
+`
+
+const ThirdCardList = styled.div`
+position: absolute;
+right: 245px;
+top: 815px;
+width: 606px;
+height: 243px;
+
+
+h3{
+font-weight: 500;
+font-size: 48px;
+color: #006837;
+margin-bottom: 40px;
+
+display:flex;
+align-items:center;
+justify-content: flex-start;
+gap:24px;
+}
+ul{
+list-style-type: inherit;
+padding-left: 50px;
+}
+
+li{
+font-weight: 400;
+font-size: 24px;
+color: #333;
+}
+
+@media (max-width: 1439px) {
+width: 534px;
+
+right: 25px;
+top: 650px;
+
+h3 {
+font-size: 32px;
+margin-bottom: 24px;
+}
+
+li{
+font-size: 20px;
+}
+}
+@media (max-width: 743px){
+width: 263px;
+height: 319px;
+
+right: 33px;
+top: 620px;
+
+h3 {
+font-size: 20px;
+margin-bottom: 16px;
+gap: 20px;
+
+svg{
+width: 32px;
+height: 32px;
+}
+}
+ul{
+padding-left: 25px;
+}
+li{
+font-size: 14px;
+}
+}
+`
+
+const FourthCardList = styled.div`
+position: absolute;
+left: 37px;
+bottom: 250px;
+width: 723px;
+height: 185px;
+
+
+h3{
+font-weight: 500;
+font-size: 48px;
+color: #006837;
+margin-bottom: 40px;
+
+display:flex;
+align-items:center;
+justify-content: flex-start;
+gap:24px;
+
+}
+ul{
+list-style-type: inherit;
+padding-left: 50px;
+}
+
+li{
+font-weight: 400;
+font-size: 24px;
+color: #333;
+}
+@media (max-width: 1439px) {
+width: 534px;
+
+left: 0px;
+top: 870px;
+
+h3 {
+font-size: 32px;
+margin-bottom: 24px;
+}
+
+li{
+font-size: 20px;
+}
+}
+
+@media (max-width: 743px){
+width: 263px;
+height: 319px;
+
+left: 0;
+top: 840px;
+
+h3 {
+font-size: 20px;
+margin-bottom: 16px;
+gap: 20px;
+
+svg{
+width: 32px;
+height: 32px;
+}
+}
+ul{
+padding-left: 25px;
+}
+li{
+font-size: 14px;
+}
+}
+
+
+
+`
+
+const FifthCardList = styled.div`
+position: absolute;
+left: 37px;
+bottom: 0px;
+width: 1018px;
+height: 185px;
+
+
+
+h3{
+font-weight: 500;
+font-size: 48px;
+color: #006837;
+margin-bottom: 40px;
+
+display:flex;
+align-items:center;
+justify-content: flex-start;
+gap:24px;
+
+}
+ul{
+list-style-type: inherit;
+padding-left: 50px;
+
+}
+
+li{
+font-weight: 400;
+font-size: 24px;
+color: #333;
+}
+
+@media (max-width: 1439px) {
+width: 534px;
+
+
+left: 0px;
+bottom: 110px;
+
+h3 {
+font-size: 32px;
+margin-bottom: 24px;
+}
+
+li{
+font-size: 20px;
+}
+}
+@media (max-width: 743px){
+width: 263px;
+
+
+left: 0px;
+bottom: -15px;
+
+h3 {
+font-size: 20px;
+margin-bottom: 16px;
+gap: 20px;
+
+svg{
+width: 32px;
+height: 32px;
+}
+}
+ul{
+padding-left: 25px;
+}
+li{
+font-size: 14px;
+}
+}
+`
+
+
+const BtnLeaveAppl = styled.button`
+
+margin: 60px auto 0 auto;
+border:none;
+border-radius: 5px;
+padding: 20px 24px;
+width: 330px;
+height: 60px;
+box-shadow: 0 1px 1px 0 rgba(30, 30, 30, 0.15);
+background-color: #006837;
+
+font-weight: 500;
+font-size: 20px;
+line-height: 1.2;
+letter-spacing: -0.02em;
+color: #fff;
+
+display: flex;
+align-items: center;
+justify-content: center;
+
+ &:hover {
+        background: #25a02a;
+        color: #fff;
+    }
+@media (max-width: 1439px){
+}
+
+@media (max-width: 743px){
+  width: 288px;
+  height: 60px;
+  margin: 24px auto 0;
+}
+
+`
